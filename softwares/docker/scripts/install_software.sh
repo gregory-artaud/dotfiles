6aaf82d9a09c5d20872ad2a42058d51b6f99afdb
Gregory ARTAUD <gregory.rtd@gmail.com>
2023-06-08 18:53:00 +0200

    docker install

---
+++ softwares/docker/scripts/install_software.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+# Prerequisities
+sudo apt install ca-certificates curl gnupg
+
+# Add GPG Key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
