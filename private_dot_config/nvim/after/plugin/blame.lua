9b47904d21218770180efad0be282238d6f28a96
Grégory <gregory@gra.sh>
2026-04-05 23:02:34 +0200

    Update .gitconfig Update .zshrc Update .config/i3/config Update .config/nvim/after/plugin/blame.lua Update .config/nvim/after/plugin/harpoon.lua Update .config/nvim/after/plugin/telescope.lua Update .config/nvim/after/plugin/treesitter.lua Update .ssh/config

--- private_dot_config/nvim/after/plugin/blame.lua
+++ private_dot_config/nvim/after/plugin/blame.lua
@@ -1,4 +1,9 @@
-require('gitblame').setup {
+local ok, gitblame = pcall(require, 'gitblame')
+if not ok then
+    return
+end
+
+gitblame.setup {
     enabled = true,
     message_template = "\t\t<author>, <date> • <summary>",
     date_format = "%r",
